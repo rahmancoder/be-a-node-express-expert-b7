ce3f4ca1000712ad2f6e05da64e02e89c0856c07
abedinforhan <abedinforhan@gmail.com>
2026-05-17 18:39:56 +0600

    module-9

--- express/src/app.ts
+++ express/src/app.ts
@@ -1,4 +1,5 @@
 import CookieParser from "cookie-parser";
+import cors from "cors";
 import express, {
   type Application,
   type Request,
@@ -8,7 +9,7 @@ import logger from "./middleware/logger";
 import { authRoute } from "./modules/auth/auth.route";
 import { profileRoute } from "./modules/profile/profile.route";
 import { userRoute } from "./modules/user/user.route";
-
+import globalErrorHandler from "./middleware/globalErrorHandler";
 const app: Application = express();
 
 app.use(CookieParser());
@@ -16,6 +17,12 @@ app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({ extended: true }));
 app.use(logger);
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  }),
+);
 
 app.get("/", (req: Request, res: Response) => {
   //res.send("Hello World!");
@@ -28,4 +35,8 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/api/users", userRoute);
 app.use("/api/profile", profileRoute);
 app.use("/api/auth", authRoute);
+
+// Global Error Handling Middleware
+app.use(globalErrorHandler);
+
 export default app;
--- express/src/modules/user/user.controller.ts
+++ express/src/modules/user/user.controller.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from "express";
+import sendResponse from "../../utility/sendResponse";
 import { userService } from "./user.service";
 
 const createUser = async (req: Request, res: Response) => {
@@ -9,13 +10,15 @@ const createUser = async (req: Request, res: Response) => {
     const result = await userService.createUserIntoDB(req.body);
     // console.log(result);
 
-    res.status(201).json({
+    sendResponse(res, {
+      statusCode: 201,
       success: true,
       message: "User Created successfully!",
       data: result.rows[0],
     });
   } catch (error: any) {
-    res.status(500).json({
+    sendResponse(res, {
+      statusCode: 500,
       success: false,
       message: error.message,
       error: error,
